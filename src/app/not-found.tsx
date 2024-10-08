a8c38f36811a22b2220ecad88119a3f5198acdcf
Luccas Rodrigues <luccasro98@gmail.com>
2024-10-08 15:02:53 +0100

    improve not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { HighlightsCarousel } from "@/components/editorial/highlights-carousel";
+import { PageContent } from "@/components/shared/page-content";
+import { Button } from "@/components/ui/button";
+import { pageRoutes } from "@/utils/routes";
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      Not Found
+    <main className="min-h-screen mt-8">
+      <PageContent>
+        <h1 className="font-bold uppercase italic pb-6 text-xl md:text-2xl lg:text-3xl">
+          Page not found
+        </h1>
+        <p>
+          The page you&apos;re looking for doesn&apos;t exist or may have been
+          removed.
+        </p>
+        <Button className="mt-6 mb-12" asChild>
+          <Link href={pageRoutes.home}>Go to Homepage</Link>
+        </Button>
+      </PageContent>
+      <HighlightsCarousel />
     </main>
   );
 }
